bf58dfd5b2c6aed8da5b686a179fa533d370060e
kaanarslan1990 <oguzkaanarslan@outlook.com>
2023-01-03 17:10:17 +0300

    add update and delete req.

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Req, Request} from '@nestjs/common';
+import { Controller, Get, Post, Req, Body, Param, Request} from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
+    // GET  /cars  => gets a list of cars
     @Get()
     findAll(@Req() request: Request): {} {
         return [{make: 'honda', model: 'accord'},
@@ -9,8 +10,44 @@ export class CarsController {
             {make: 'fiat', model: '123 spider'}];
     }
 
-    @Get('showcase')
-    showcase(@Req() request: Request): string {
-        return 'This is the cars showcase';
+    // POST /cars => creates a new car based on the bundle of data you sent it
+    @Post()
+    async create(@Body() carParams) {
+        return `I got your post request ! 
+    You want to create a ${carParams.make}`;
     }
+
+    // GET /cars/:id => gets one car based on the id you sent it as a wildcard
+    @Get(':id')
+    findOne(@Req() request: Request): {} {
+        return {id: 25, make: 'tesla', model: 'model x'}
+    }
+
+
+    // POST /cars/:id => updates a car based on a bundle of data to update a car
+    @Post(':id')
+    async update(@Body() carParams, @Param() params) {
+        return `I got your post request ! 
+    You want to edit a ${carParams.make} belonging to
+    ${params.id}`;
+    }
+
+    // POST /cars/:id/delete => deletes a car... all you need is an ID !
+    // we only need an ID because that's all we need to send to the database
+    // to identify the record to delete
+    @Post(':id/delete')
+    async delete(@Param() params) {
+        return `planning to delete ${params.id}`;
+    }
+
+
 }
+
+
+/*
+GET  /cars  => gets a list of cars
+POST /cars => creates a new car based on the bundle of data you sent it
+GET /cars/:id => gets one car based on the id you sent it as a wildcard
+POST /cars/:id => updates a car based on a bundle of data to update a car
+POST /cars/:id/delete => deletes a car
+*/
